0f3890fccb5f145adf1671490db72a3d47982f7a
RobertHJones <roberthuwjones@outlook.com>
2022-01-11 13:43:40 +0000

    priority-list-start

---
+++ src/components/App/index.js
@@ -0,0 +1,29 @@
+import "./index.css";
+import { useState, useEffect } from "react";
+import List from "../List/index.js";
+import Input from "../Input";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  function addItem(item) {
+    // Don't add the item if there is already one the same that exists
+    if (todos.includes(item)) {
+      return;
+    }
+    setTodos([...todos, item]);
+  }
+
+  function removeItem(index) {
+    setTodos([...todos.slice(0, index), ...todos.slice(index + 1)]);
+  }
+
+  return (
+    <div className="App">
+      <Input onSubmitClick={addItem} />
+      <List items={todos} onItemDeleteClick={removeItem} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Input/index.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import "./index.css";
+
+function Input({ onSubmitClick }) {
+  const [text, setText] = useState("");
+
+  function handleChange(event) {
+    setText(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    // Stop the page from refreshing as the form will try and submit and refresh by default
+    event.preventDefault();
+    onSubmitClick(text);
+  }
+
+  return (
+    <form>
+      <input onChange={handleChange} />
+      <button onClick={handleSubmit} value={text}>
+        Submit Standard Priority Task
+      </button>
+      <button
+        id="highpriority"
+        onClick={handleSubmit}
+        value={text.toUpperCase()}
+      >
+        Submit High Priority Task
+      </button>
+    </form>
+  );
+}
+
+export default Input;
